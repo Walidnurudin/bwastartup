357a80dd7529de558f15779f87b4126289198bdb
Walidnurudin <walidnurudin47@gmail.com>
2021-02-10 04:42:03 +0700

    get data from database

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bwastartup/user"
 	"fmt"
 	"log"
 
@@ -11,11 +12,19 @@ import (
 func main() {
 	// refer https://github.com/go-sql-driver/mysql#dsn-data-source-name for details
 	dsn := "root:@tcp(127.0.0.1:3306)/bwastartup?charset=utf8mb4&parseTime=True&loc=Local"
-	_, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 
 	if err != nil {
 		log.Fatal(err.Error())
 	}
 
 	fmt.Println("Connection to database is Lancar jaya")
+
+	var users []user.User
+
+	db.Find(&users)
+
+	for index, data := range users {
+		fmt.Println(index+1, data.Name)
+	}
 }
