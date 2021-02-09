9ebb48e98e701df4d6290f47ff27605a5aa9a3d3
Walidnurudin <walidnurudin47@gmail.com>
2021-02-10 05:09:52 +0700

    struktur folder & file

--- main.go
+++ main.go
@@ -4,12 +4,23 @@ import (
 	"bwastartup/user"
 	"fmt"
 	"log"
+	"net/http"
 
+	"github.com/gin-gonic/gin"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
 
 func main() {
+	router := gin.Default()
+	router.GET("/", func(c *gin.Context) {
+		c.String(http.StatusOK, "Sedang belajar Golang")
+	})
+	router.GET("/handler", handler)
+	router.Run()
+}
+
+func handler(c *gin.Context) {
 	// refer https://github.com/go-sql-driver/mysql#dsn-data-source-name for details
 	dsn := "root:@tcp(127.0.0.1:3306)/bwastartup?charset=utf8mb4&parseTime=True&loc=Local"
 	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
@@ -21,10 +32,7 @@ func main() {
 	fmt.Println("Connection to database is Lancar jaya")
 
 	var users []user.User
-
 	db.Find(&users)
 
-	for index, data := range users {
-		fmt.Println(index+1, data.Name)
-	}
+	c.JSON(http.StatusOK, users)
 }
